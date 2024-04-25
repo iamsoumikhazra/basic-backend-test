e2d67a5aba4d06830181448266d02d48380101a1
iamsoumikhazra <www.soumikhazra@gmail.com>
2024-04-25 10:41:47 -0500

    handling github api in /github

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const app = express()
 const port = process.env.PORT || 5000
 const insta = "iamsoumikhazra";
 
+const github = "https://api.github.com/users/iamsoumikhazra" ;
 
+const axios =require('axios')
 
 app.get('/',(req,res)=>{
   res.send(`sending hello from world\'s backend <br/>
@@ -19,7 +21,27 @@ app.get('/insta',(req,res)=>{
 })
 app.get('/google',(req,res)=>{
   res.send(`GO TO<a href="https://www.google.com/" target="_blank">Google</a>`)
+  
 })
+
+
+
+app.get('/github', async (req, res) => {
+  try {
+    const response = await axios.get(github);
+    const avatarUrl = response.data.avatar_url;
+    const name = response.data.name;
+    const followers = response.data.followers;
+    res.send(`<img src="${avatarUrl}" alt="avatar" /> </br> <h2>${name} </br> Followers:${followers}</h2>`);
+    
+    // res.send(`<h2>name:${name}:${followers}</h2>`);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+
+
 
 app.listen(port,()=>{
   console.log(`server is running on port ${port}`)
